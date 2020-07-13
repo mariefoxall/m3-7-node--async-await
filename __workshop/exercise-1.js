83623962e9b8550f90c3746793510e8b433bf9fa
Marie Foxall <marie@wastedeffort.ca>
2020-07-13 11:17:56 -0400

    1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -3,23 +3,30 @@
 
 const makeAllCaps = (array) => {
   return new Promise((resolve, reject) => {
-    array.every((word) => typeof word === 'string')
+    array.every((word) => typeof word === "string")
       ? resolve(array.map((word) => word.toUpperCase()))
-      : reject('Error: Not all items in the array are strings!');
+      : reject("Error: Not all items in the array are strings!");
   });
 };
 
 const sortWords = (array) => {
   return new Promise((resolve, reject) => {
-    array.every((word) => typeof word === 'string')
+    array.every((word) => typeof word === "string")
       ? resolve(array.sort())
-      : reject('Error: Something went wrong with sorting words.');
+      : reject("Error: Something went wrong with sorting words.");
   });
 };
 
 const textTransform = async (array) => {
-  // here
+  try {
+    const capsArray = await makeAllCaps(array);
+    const sortedArray = await sortWords(capsArray);
+    console.log(sortedArray);
+    return sortedArray;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-textTransform(['cucumber', 'tomatos', 'avocado']);
-textTransform(['cucumber', 44, true]);
+textTransform(["cucumber", "tomato", "avocado"]);
+textTransform(["cucumber", 44, true]);
