366f7773dde33bf6a2514b8dae2992770f798359
Marie Foxall <marie@wastedeffort.ca>
2020-07-13 13:53:16 -0400

    4.1

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -1,6 +1,24 @@
-const request = require('request-promise');
+const request = require("request-promise");
+const requestPromise = require("request-promise");
+const options = {
+  uri: "https://icanhazdadjoke.com/",
+  headers: {
+    "User-Agent": "Request-Promise",
+    Accept: "application/json",
+  },
+  json: true, // Automatically parses the JSON string in the response
+};
+const getDadJoke = async () => {
+  try {
+    const jokeObject = await request(options);
+    // console.log(jokeObject);
+    return jokeObject.joke;
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+};
 
-// getDadJoke
+getDadJoke().then((data) => console.log(data));
 
 // 4.1
 // getJoke().then((data) => console.log(data));
