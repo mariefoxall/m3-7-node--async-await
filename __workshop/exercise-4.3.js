f8b713d3589814eeb6481ee1c5cf320b7cf03bf4
Marie Foxall <marie@wastedeffort.ca>
2020-07-13 15:28:57 -0400

    4.3

--- __workshop/exercise-4.3.js
+++ __workshop/exercise-4.3.js
@@ -1,5 +1,17 @@
-const request = require('request-promise');
+const request = require("request-promise");
+const options = {
+  uri: "https://geek-jokes.sameerkumar.website/api?format=json",
+  json: true,
+};
 
 const getGeekJoke = async () => {
   // ha hahahh
+  try {
+    const randomGeekJoke = await request(options);
+    return randomGeekJoke.joke;
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+getGeekJoke().then((data) => console.log(data));
