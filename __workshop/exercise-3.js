338f91a1735f6488cdaf9167af156069fef4a185
Marie Foxall <marie@wastedeffort.ca>
2020-07-13 13:18:17 -0400

    3

--- __workshop/exercise-3.js
+++ __workshop/exercise-3.js
@@ -2,11 +2,32 @@
 // ----------
 
 const doublesLater = (num) => {
-  // 1. waits 2 seconds (You could console a message here. It might make the 2 second wait easier ;)
-  // 2. returns the double of the num
+  return new Promise((resolve, reject) => {
+    // console.log(num);
+    if (typeof num === "number") {
+      console.log("wait for it...");
+      setTimeout(() => {
+        const doubleNum = num * 2;
+        // console.log("num", num);
+        resolve(doubleNum);
+      }, 2000);
+    } else {
+      reject("not a number");
+    }
+  });
 };
 
 // 3. handleSum function (async/await)
+const handleSum = async (num) => {
+  try {
+    const step1 = await doublesLater(num);
+    const step2 = await doublesLater(step1);
+    const step3 = await doublesLater(step2);
+    return step1 + step2 + step3;
+  } catch (error) {
+    console.log("error: " + error);
+  }
+};
 
 // 4. verification
 handleSum(10).then((ans) => console.log(ans));
